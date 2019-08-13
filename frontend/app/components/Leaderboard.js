6c4d921291f73d381a9f43f03785204bc02bc706
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-08-13 12:13:36 -0700

    update koji-tools, game-asset-loader

--- frontend/app/components/Leaderboard.js
+++ frontend/app/components/Leaderboard.js
@@ -21,7 +21,6 @@ class Leaderboard extends Component {
   }
 
   componentDidMount() {
-    console.log('leaderboard view');
 
     fetch(`${Koji.config.serviceMap.backend}/leaderboard`)
       .then((response) => response.json())
